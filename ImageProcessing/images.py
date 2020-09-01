5b21b27a8411b3a4ff7facb4c6156796414ae02d
August590 <45698699+August590@users.noreply.github.com>
2020-09-01 14:11:37 -0500

    ImageConverterJPGPNG

---
+++ ImageProcessing/images.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageFilter
+
+# img = Image.open('./Pokedex/pikachu.jpg')
+# filtered_img = img.convert('L')
+
+# print(img)
+# box = (100, 100, 200, 200)
+# region = filtered_img.crop(box)
+# region.save("grey.png", 'png')
+# crooked = filtered_img.rotate(90)
+# crooked.save("grey.png", 'png')
+# crooked.show()
+
+# img = Image.open('./astro.jpg')
+# img.thumbnail((400, 400))
+# img.save('thumbnail.jpg')
