c97b95a7bf5b630348df34bdcb142c4bf3bae025
August590 <45698699+August590@users.noreply.github.com>
2020-09-06 15:15:34 -0500

    Watermarker
    Puts a watermark on all the pages of a pdf

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -1,11 +1,22 @@
 import PyPDF2
 
-with open('dummy.pdf', 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    page = reader.getPage(0)
-    print(reader.numPages)
-    print(page.rotateCounterClockwise(90))
-    writer = PyPDF2.PdfFileWriter()
-    writer.addPage(page)
-    with open('tilt.pdf', 'wb') as newfile:
-        writer.write(newfile)
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for page in range(template.getNumPages()):
+    page = template.getPage(page)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+with open('watermarked_output.pdf', 'wb') as file:
+    output.write(file)
+# with open('dummy.pdf', 'rb') as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     page = reader.getPage(0)
+#     print(reader.numPages)
+#     print(page.rotateCounterClockwise(90))
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+#     with open('tilt.pdf', 'wb') as newfile:
+#         writer.write(newfile)
