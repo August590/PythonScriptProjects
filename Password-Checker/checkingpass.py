b6b5baef53c2bd219b92cba8cc4ca8944af1d597
August590 <45698699+August590@users.noreply.github.com>
2020-09-09 05:08:11 -0500

    Create checkingpass.py

---
+++ Password-Checker/checkingpass.py
@@ -0,0 +1,34 @@
+import requests
+import hashlib
+
+url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
+res = requests.get(url)
+print(res)
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'Error feathcing: {res.status_code}, check the api and try again')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        print(h, count)
+
+
+def pwned_api_check(password):
+    # print(password.encode('utf-8'))
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    # print(first5_char, tail)
+    print(response)
+    return get_password_leaks_count(response, tail)
+
+
+pwned_api_check('123')
