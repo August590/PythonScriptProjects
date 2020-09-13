2fb8545ea3770b4e680a368f2761c4084416566f
August590 <45698699+August590@users.noreply.github.com>
2020-09-13 00:22:05 -0500

    Update tweet.py
    Changed the keys so that they are hidden

--- TwitterBot/tweet.py
+++ TwitterBot/tweet.py
@@ -1,16 +1,24 @@
 import tweepy
 
 
-def getKeyNTokens(doc):
-    with open('test.txt', mode='r') as my_file:
+def getKeysNTokens(doc):
+    with open(doc, mode='r') as my_file:
         # print(my_file.read())
-        Lines = my_file.readlines()
-        print(Lines)
-        return Lines
+        # Lines = my_file.readlines()
+        my_lines = []
+        for lines in my_file:
+            my_lines.append(lines.strip())
+
+        # print(Lines)
+        return my_lines
 
 
-# api = tweepy.API(auth)
+keys = getKeysNTokens(r'C:\Users\Augus\OneDrive\Desktop\keys.txt')
+auth = tweepy.OAuthHandler(keys[0], keys[1])
+auth.set_access_token(keys[2], keys[3])
 
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
